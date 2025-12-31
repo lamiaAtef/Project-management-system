7acc8d87e1e21e3898be621eb43dba0f45c7ded2
lamiaAtef <90112559+lamiaAtef@users.noreply.github.com>
2025-12-31 11:34:37 +0200

    Fix AuthLayout images overlay issue

--- src/Modules/AuthModule/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Modules/AuthModule/Components/ForgetPassword/ForgetPassword.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import AuthHeader from '../../../Shared/components/AuthHeader/AuthHeader';
+import Form from 'react-bootstrap/Form';
+import { EMAIL_VALIDATION } from '../../../../services/validation';
+
 
 export default function ForgetPassword() {
+   const colProps = { md: 8, lg: 6, xl: 5 };
+   const{register, formState:{errors}, handleSubmit}= useForm();
+   let navigate = useNavigate();
+    const onSubmit=(data)=>{
+      console.log(data);
+    }
+
   return (
     <>
-      ForgetPassword
+      <Col {...colProps} className=" p-3 rounded-3 formBg text-white">
+      <AuthHeader subtitle={'welcome to PMS'} title={'Forget Password'}/>       
+        <Form onSubmit={handleSubmit(onSubmit)} className='m-5'>
+          <Form.Group className="custom-input mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className='textHeader'>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="Enter your E-mail" 
+               {...register('email',EMAIL_VALIDATION)} />
+               {errors.email && <small className='text-danger d-block mt-1'>{errors.email.message}</small>}
+          </Form.Group>
+          <Button type='submit' className='w-100 mt-4 Auth-btn'>Login</Button>
+          
+         </Form>
+      </Col>
     </>
   )
 }
--- src/Modules/Shared/components/AuthLayout/AuthLayout.jsx
+++ src/Modules/Shared/components/AuthLayout/AuthLayout.jsx
@@ -41,7 +41,7 @@ export default function AuthLayout() {
        <img src={images.img2} alt="auth visual 2" className={styles.position_img2_authLayout} />
 
       {/* wrapper content */}
-      <Row className="d-flex justify-content-center align-items-center h-100 flex-column  " style={{ zIndex: 10 }}>
+      <Row className={`d-flex justify-content-center align-items-center h-100 flex-column ${styles.content} `}>
          <img src={logo} alt="" className='w-25 mb-3' />
           <Outlet />
         
